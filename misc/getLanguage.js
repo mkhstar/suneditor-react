7ef58276087658d7b94b51cb928ad84436855a1a
Kusi Musah Hussein <kusimusah@gmail.com>
2019-11-26 23:13:31 +0300

    fixed bug of get lang

--- misc/getLanguage.js
+++ misc/getLanguage.js
@@ -3,24 +3,7 @@ const getLanguage = lang => {
     case 'object':
       return lang;
     case 'string':
-      switch (lang) {
-        case 'da':
-          return require('suneditor/src/lang/da');
-        case 'de':
-          return require('suneditor/src/lang/de');
-        case 'en':
-          return require('suneditor/src/lang/en');
-        case 'fr':
-          return require('suneditor/src/lang/fr');
-        case 'ja':
-          return require('suneditor/src/lang/ja');
-        case 'ko':
-          return require('suneditor/src/lang/ko');
-        case 'ru':
-          return require('suneditor/src/lang/ru');
-        case 'zh_cn':
-          return require('suneditor/src/lang/zh_cn');
-      }
+      return require('suneditor/src/lang')[lang];
       break;
   }
   return require('suneditor/src/lang/en');
