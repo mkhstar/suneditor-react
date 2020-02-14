f920608bc113ba3ea97b5df1604fb15c30f4d42d
Kusi Musah Hussein <kusimusah@gmail.com>
2020-02-14 12:22:01 +0300

    switched to webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./main.js",
+  output: {
+    path: path.resolve("dist"),
+    filename: "main.js",
+    libraryTarget: "commonjs2"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
+  },
+  externals: {
+    react: "react"
+  }
+};
