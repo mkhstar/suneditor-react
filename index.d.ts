85bc7a18aff2129aaeadf57d8cd1b47317793639
Kusi Musah Hussein <kusimusah@gmail.com>
2020-06-20 22:42:28 +0300

    typescript fixes

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import SetOptions from './types/SetOptions';
-import SunEditor from './SunEditor';
 
 export interface SunEditorReactProps {
   onChange?: (content: string) => void;
